bd4f49ef26f88ae13d5b7a971ce435ead96147d0
sushantubale <suubale@duosecurity.com>
2023-07-25 10:51:24 -0500

    Added XCode project

---
+++ LocalizationFramework/LocalizationFramework/LocalizationFramework.h
@@ -0,0 +1,18 @@
+//
+//  LocalizationFramework.h
+//  LocalizationFramework
+//
+//  Created by suubale on 7/25/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalizationFramework.
+FOUNDATION_EXPORT double LocalizationFrameworkVersionNumber;
+
+//! Project version string for LocalizationFramework.
+FOUNDATION_EXPORT const unsigned char LocalizationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalizationFramework/PublicHeader.h>
+
+
